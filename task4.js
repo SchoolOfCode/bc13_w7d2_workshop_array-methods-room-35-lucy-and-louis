1fd29df2a1f828567dd4929a905a4764b80e2b3c
L-McHugh <l.mchugh1@outlook.com>
2022-11-08 13:30:26 +0000

    task 4 complete and task 6 almost complete

--- task4.js
+++ task4.js
@@ -7,3 +7,14 @@ let seventySevenTimesTable = [77, 154, 231, 308, 385, 461, 538, 616, 693, 770];
 const multiple = sevenTimesTable.every((num) => {
     return num % 7 === 0 
 })
+
+const ssMultiple = seventySevenTimesTable.every((nums) => {
+    return nums % 77 === 0
+})
+
+const findBad = seventySevenTimesTable.find((nums) => {
+    return nums % 77 != 0
+})
+
+const findIndex = seventySevenTimesTable.findIndex((nums) => {return nums % 77 != 0})
+console.log(findIndex)
--- task6.js
+++ task6.js
@@ -1,11 +1,17 @@
 const broomCupboard = [2, 4, 1, 3, 7, 5, 8, 6, 10, 9, 0];
 
+const sorting = broomCupboard.sort((a, b) => a - b)
+console.log(sorting)
+
 const potions = [
   { name: "polyjuice potion", shelf: 2 },
   { name: "draught of living death", shelf: 4 },
   { name: "pepperup potion", shelf: 1 },
   { name: "veritaserum", shelf: 3 },
 ];
+
+const sortingShelf = potions.sort((b, a) => a.shelf - b.shelf)
+console.log(sortingShelf)
 
 const students = [
   "Cedric Diggory",
@@ -16,3 +22,9 @@ const students = [
   "Luna Lovegood",
   "Gregory Goyle",
 ];
+
+const splitting = students[i].split(" ")
+//const lastName = students.sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1))
+console.log (splitting)
+
+//console.log(lastName)
