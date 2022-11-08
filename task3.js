c5bdcd30ae3fe4b45d57acd1f6b648dda5e0cf0d
LouisWright <louis--wright@hotmail.co.uk>
2022-11-08 12:29:52 +0000

    completed task 3 and part 1 of task 4

--- task3.js
+++ task3.js
@@ -1,1 +1,7 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+const multiple = someNumbers.some((mul) => {
+    return mul % 3 === 0 || mul % 5 === 0 || mul % 60 === 0 || mul % 90 === 0
+})
+console.log(multiple)
+
--- task4.js
+++ task4.js
@@ -1,3 +1,9 @@
 let sevenTimesTable = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
 
 let seventySevenTimesTable = [77, 154, 231, 308, 385, 461, 538, 616, 693, 770];
+
+
+
+const multiple = sevenTimesTable.every((num) => {
+    return num % 7 === 0 
+})
